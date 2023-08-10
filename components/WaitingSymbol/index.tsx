a3bb927beeb50f1d29a04f5fe87e75f13d8d5490
estellechoi <estele.choi@gmail.com>
2023-08-11 01:12:02 +0900

    feat: modify WaitingSymbol to have 4 units

--- components/WaitingSymbol/index.tsx
+++ components/WaitingSymbol/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import WaitingSymbolUnit from './WaitingSymbolUnit';
 import type { WaitingSymbolColor } from './types';
 
@@ -7,26 +6,13 @@ type WaitingSymbolProps = {
 };
 
 const WaitingSymbol = ({ color = 'white' }: WaitingSymbolProps) => {
-  const [is2ndStarted, setIs2ndStarted] = useState(false);
-  const [is3rdStarted, setIs3rdStarted] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setIs2ndStarted(true), 200);
-    setTimeout(() => setIs3rdStarted(true), 400);
-  }, []);
-
   return (
     <div className="flex justify-center items-center gap-x-0">
       <WaitingSymbolUnit color={color} className="animate-bouncing" />
       <WaitingSymbolUnit color={color} className="animate-bouncing_delayed_1" />
       <WaitingSymbolUnit color={color} className="animate-bouncing_delayed_2" />
       <WaitingSymbolUnit color={color} className="animate-bouncing_delayed_3" />
-      <WaitingSymbolUnit color={color} className="animate-bouncing_delayed_4" />
-
-      {/* <WaitingSymbolUnit color={color} className={is2ndStarted ? 'animate-bouncing_delayed_1' : ''} />
-      <WaitingSymbolUnit color={color} className={is3rdStarted ? 'animate-bouncing_delayed_2' : ''} />
-      <WaitingSymbolUnit color={color} className={is3rdStarted ? 'animate-bouncing_delayed_3' : ''} />
-      <WaitingSymbolUnit color={color} className={is3rdStarted ? 'animate-bouncing_delayed_4' : ''} /> */}
+      {/* <WaitingSymbolUnit color={color} className="animate-bouncing_delayed_4" /> */}
     </div>
   );
 };
